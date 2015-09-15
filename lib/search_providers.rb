fc368fa58bd90dbfa37aa28a0a235e182f9e0e1c
arnkorty <arnkorty.fu@gmail.com>
2015-09-15 18:38:35 +0800

    --amend

--- lib/search_providers.rb
+++ lib/search_providers.rb
@@ -1,4 +1,4 @@
-require Rails.join('lib/search_providers/_base').to_s
+require Rails.root.join('lib/search_providers/_base').to_s
 Dir[Rails.root.join('lib/search_providers/*')].each do |path|
   Rails.configuration.cache_classes ? require(path) : load(path)
 end
