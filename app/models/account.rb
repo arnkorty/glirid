02ddb2d3f39de11a5fe370583053b2fb942924e9
arnkorty <arnkorty.fu@gmail.com>
2015-09-15 19:06:07 +0800

    fix account validate fix

--- app/models/account.rb
+++ app/models/account.rb
@@ -12,7 +12,7 @@ class Account < ActiveRecord::Base
   has_many :search_tasks, through: :searches
 
   validates :email,
-    :presence => true, format: {with: email_regexp},
+    :presence => true, format: {with: email_regexp}
   validates_uniqueness_of :email, scope: :deleted_at, unless: :deleted?
 
   before_create :set_rss_token
