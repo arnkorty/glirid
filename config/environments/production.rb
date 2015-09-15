7603fede079a17ddfb519ccdf393e37db1717d67
arnkorty <arnkorty.fu@gmail.com>
2015-09-15 19:11:03 +0800

    add production env email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { from: 'no-reply@glirid.com', host: 'http://glirid.dev.enjoyways.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = Rails.application.secrets.smtp
+ 
 end
