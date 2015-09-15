5c04bc6170221a93505190a2e4e910dbb05ea0d1
arnkorty <arnkorty.fu@gmail.com>
2015-09-15 18:40:31 +0800

    delete bad migration file

--- db/migrate/20150913112353_add_account_to_rss_feeds.rb
+++
@@ -1,5 +0,0 @@
-class AddAccountToRssFeeds < ActiveRecord::Migration
-  def change
-    add_reference :rss_feeds, :account, index: true, foreign_key: true
-  end
-end
--- db/migrate/20150914172334_add_description_to_rss_feeds.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToRssFeeds < ActiveRecord::Migration
-  def change
-    add_column :rss_feeds, :description, :text
-  end
-end
