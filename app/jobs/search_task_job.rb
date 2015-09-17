18fa9d1b4f72b54b6c6a6cae2c9980cfd3f11ed5
arnkorty <arnkorty.fu@gmail.com>
2015-09-17 21:44:03 +0800

    fix search tasks

--- app/jobs/search_task_job.rb
+++ app/jobs/search_task_job.rb
@@ -3,7 +3,7 @@ class SearchTaskJob < JobStatusable
     task = SearchTask.find task_id
     task.search.sync_run
     if task.frequency.frequency_time > 0
-      # SearchTaskJob.set(wait: task.frequency.frequency_time).perform_later task_id
+      SearchTaskJob.set(wait: task.frequency.frequency_time).perform_later task_id
     end
   end
 end
